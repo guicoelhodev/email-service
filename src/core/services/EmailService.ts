6bb3fa2de63d2bfc27826a6004efc8a5f28bc711
guicoelhodev <gs.coelho_dev@outlook.com>
2025-07-28 10:46:45 -0300

    refactor: add dynamic import from typescript

--- src/core/services/EmailService.ts
+++ src/core/services/EmailService.ts
@@ -1,24 +1,24 @@
-import { Email, EmailData } from "../models/Email";
-import { QueueService } from "../ports/QueueService";
+import { Email, EmailData } from "@/core/models/Email";
+import { QueueService } from "@/core/ports/QueueService";
 
 type SendEmail = Omit<EmailData, "createdAt" | "id">;
 
 export class EmailService {
-	constructor(private queueService: QueueService) { }
+  constructor(private queueService: QueueService) {}
 
-	async sendEmail(emailData: SendEmail) {
-		const emailId = crypto.randomUUID();
+  async sendEmail(emailData: SendEmail) {
+    const emailId = crypto.randomUUID();
 
-		const email = new Email({
-			id: emailId,
-			createdAt: new Date(),
-			body: emailData.body,
-			subject: emailData.subject,
-			to: emailData.to,
-		});
+    const email = new Email({
+      id: emailId,
+      createdAt: new Date(),
+      body: emailData.body,
+      subject: emailData.subject,
+      to: emailData.to,
+    });
 
-		await this.queueService.addJob("email-job", email.getEmailData());
+    await this.queueService.addJob("email-job", email.getEmailData());
 
-		return emailId;
-	}
+    return emailId;
+  }
 }
