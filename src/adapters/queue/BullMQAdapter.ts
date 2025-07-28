3d8f4a7a7c56391a668062e4e638ca72c1eb66bb
guicoelhodev <gs.coelho_dev@outlook.com>
2025-07-28 10:57:44 -0300

    feat(Adapter): create BullMQAdapter

---
+++ src/adapters/queue/BullMQAdapter.ts
@@ -0,0 +1,19 @@
+import { QueueService } from "@/core/ports/QueueService";
+import { Queue } from "bullmq";
+
+export class BullMQAdapter implements QueueService {
+  private queue: Queue;
+
+  constructor(queueName: string) {
+    this.queue = new Queue(queueName, {
+      connection: {
+        host: process.env.REDIS_HOST,
+        port: parseInt(process.env.REDIS_PORT!),
+      },
+    });
+  }
+
+  async addJob(jobName: string, data: any) {
+    await this.queue.add(jobName, data);
+  }
+}
