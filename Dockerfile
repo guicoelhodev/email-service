0fb42787e32a13f62bcf56a52798f195b5700618
guicoelhodev <gs.coelho_dev@outlook.com>
2025-07-29 13:41:55 -0300

    feat(build): fix dockerfile compose to production

--- Dockerfile
+++ Dockerfile
@@ -6,13 +6,10 @@ COPY package*.json ./
 
 RUN npm ci
 
-ARG PORT
-
 COPY . .
 
 RUN npm run build
 
-ENV PORT=$PORT
 EXPOSE 3000
 
 CMD ["node", "build/server.js"]
--- src/application/Routes.ts
+++ src/application/Routes.ts
@@ -2,17 +2,24 @@ import { HttpService } from "@/core/ports/HttpService";
 import { SendEmailFactory } from "./factories/SendEmailFactory";
 
 export function setupRoutes(httpService: HttpService) {
+	const sendEmail = new SendEmailFactory().build();
+	httpService.addRoute("GET", "/", async () => {
+		return {
+			statusCode: 200,
+			body: {
+				message: "Server works correctly",
+				envs: [
+					process.env.SMTP_HOST,
+					process.env.SMTP_USER,
+					process.env.SMTP_PASS,
+					process.env.SMTP_FROM,
+					process.env.PORT,
+					process.env.REDIS_PORT,
+					process.env.REDIS_HOST,
+				],
+			},
+		};
+	});
 
-  const sendEmail = new SendEmailFactory().build();
-  httpService.addRoute("GET", "/", async () => {
-    return {
-      statusCode: 200,
-      body: {
-        message: "Server works correctly",
-      },
-    };
-  });
-
-
-  httpService.addRoute("POST", "/send-email", sendEmail.handleSendEmail);
+	httpService.addRoute("POST", "/send-email", sendEmail.handleSendEmail);
 }
