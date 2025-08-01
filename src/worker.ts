e587822b2b175d92e2484b6be2126fbd89e5bc6b
guicoelhodev <gs.coelho_dev@outlook.com>
2025-08-01 15:50:55 -0300

    refactor(worker): try to fix readonly default redis configuration

--- src/worker.ts
+++ src/worker.ts
@@ -4,23 +4,23 @@ import "dotenv/config";
 const workersManager = new WorkersManager();
 
 async function startWorkers() {
-	try {
-		await workersManager.start();
-	} catch (error) {
-		process.exit(1);
-	}
+  try {
+    await workersManager.start();
+  } catch (error) {
+    process.exit(1);
+  }
 }
 
 process.on("SIGINT", async () => {
-	console.log("Shutting down workers...");
-	await workersManager.stop();
-	process.exit(0);
+  console.log("Shutting down workers...");
+  await workersManager.stop();
+  process.exit(0);
 });
 
 process.on("SIGTERM", async () => {
-	console.log("Shutting down workers...");
-	await workersManager.stop();
-	process.exit(0);
+  console.log("Shutting down workers...");
+  await workersManager.stop();
+  process.exit(0);
 });
 
 startWorkers();
